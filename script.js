26393b353ca39b73a4aa36ac93dc02fb11879b25
PritamKamdar20-cell <pritamkamdar2.0@gmail.com>
2025-12-25 12:39:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function animateOnScroll() {
 
 window.addEventListener('scroll', animateOnScroll);
 animateOnScroll();
+
+// EMAIL SEND FUNCTION
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "YOUR_SERVICE_ID",
+    "YOUR_TEMPLATE_ID",
+    this
+  ).then(() => {
+    alert("Message sent successfully!");
+    this.reset();
+  }, (error) => {
+    alert("Failed to send message!");
+    console.log(error);
+  });
+});
