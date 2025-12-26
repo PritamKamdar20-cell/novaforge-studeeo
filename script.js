e3d77dcdb8042b31a17093730cda02a52d2010e5
PritamKamdar20-cell <pritamkamdar2.0@gmail.com>
2025-12-26 16:55:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Button click feedback
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert(`${button.innerText} coming soon!`);
+  });
+});
+
+// Navbar item click
+document.querySelectorAll(".nav-links li").forEach(item => {
+  item.addEventListener("click", () => {
+    console.log(`${item.innerText} clicked`);
+  });
+});
 
